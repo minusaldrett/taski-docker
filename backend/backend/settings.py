c5bde6252ef4ebd897ea8a3b89bb6f9a77934539
minusaldrett <konstantinsupriakov@gmail.com>
2026-04-01 18:54:11 +0400

    settings.py fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'abobabuba.servehttp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'backend', 'abobabuba.servehttp.com']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://abobabuba.servehttp.com',
